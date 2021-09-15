1bba734e8bf322a1ec9e6ce8f434c826085bb4f5
Víctor Vanaclocha Cebrián <victorvace@gmail.com>
2021-09-15 19:44:11 +0200

    Ejercicio 2

--- src/main/java/es/geekshubs/academy/Numbers.java
+++ src/main/java/es/geekshubs/academy/Numbers.java
@@ -1,9 +1,9 @@
 package es.geekshubs.academy;
 
-import java.util.ArrayList;
+import java.math.BigDecimal;
+import java.math.MathContext;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Numbers {
 
@@ -13,7 +13,17 @@ public class Numbers {
    * @return
    */
   public List<String> apply(final List<Integer> input) {
-    return null;
+    long positiveCount = input.stream().filter(integer -> integer > 0).count();
+    long negativeCount = input.stream().filter(integer -> integer < 0).count();
+    long zerosCount = input.stream().filter(integer -> integer == 0).count();
+
+    double l1 = (double) positiveCount / (double) input.size();
+    double l2 = (double) negativeCount / (double) input.size();
+    double l3 = (double) zerosCount / (double) input.size();
+
+    return Arrays.asList(String.format("%-6s", BigDecimal.valueOf(l1).round(new MathContext(4))).replace(' ', '0'),
+            String.format("%-6s", BigDecimal.valueOf(l2).round(new MathContext(4))).replace(' ', '0'),
+            String.format("%-6s", BigDecimal.valueOf(l3).round(new MathContext(4))).replace(' ', '0'));
   }
 
 }
